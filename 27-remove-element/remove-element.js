141a53d92d170b57476f24b6056c720f81819af5
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2024-08-05 21:36:16 +0300

    Time: 51 ms (71.50%) | Memory: 48.4 MB (92.08%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -4,12 +4,13 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
+    let count = 0;
 
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == val) {
-            nums.splice(i--, 1);
+        if (nums[i] !== val) {
+            nums[count] = nums[i]
+            count++
         }
     }
-    
-    return nums.length;
+    return count
 };
