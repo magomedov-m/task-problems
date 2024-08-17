ed75e8a99efd9941bbf928661c69a9abf115d488
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2024-08-17 15:10:21 +0300

    Time: 71 ms (30.70%) | Memory: 52.6 MB (49.59%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -2,30 +2,54 @@
  * @param {string[]} tokens
  * @return {number}
  */
-var evalRPN = function(A) {
-    let st = [];
+// var evalRPN = function(tokens) {
+//     let st = [];
 
-    for (let c of A) {
-        if (c == '+') {
-            let sec = +st.pop();
-            let first = +st.pop();
-            st.push(first + sec);
-        }else if (c == '-') {
-            let sec = +st.pop();
-            let first = +st.pop();
-            st.push(first - sec);
-        }else if (c == '*') {
-            let sec = +st.pop();
-            let first = +st.pop();
-            st.push(first * sec);
-        }else if (c == '/') {
-            let sec = +st.pop();
-            let first = +st.pop();
-            st.push(Math.trunc(first / sec));
+//     for (let c of A) {
+//         if (c == '+') {
+//             let sec = +st.pop();
+//             let first = +st.pop();
+//             st.push(first + sec);
+//         }else if (c == '-') {
+//             let sec = +st.pop();
+//             let first = +st.pop();
+//             st.push(first - sec);
+//         }else if (c == '*') {
+//             let sec = +st.pop();
+//             let first = +st.pop();
+//             st.push(first * sec);
+//         }else if (c == '/') {
+//             let sec = +st.pop();
+//             let first = +st.pop();
+//             st.push(Math.trunc(first / sec));
+//         }else {
+//             st.push(c);
+//         }
+//     }
+
+//     return st[0];
+// };
+
+var evalRPN = function(tokens) {
+    const stack = [];
+    
+    for (let i of tokens) {
+        if (i === '+') {
+            stack.push(stack.pop() + stack.pop())
+        }else if (i === '*') {
+            stack.push(stack.pop() * stack.pop())
+        }else if (i === '-') {
+            let first = stack.pop()
+            let sec = stack.pop()
+            stack.push(sec - first)
+        }else if (i === '/') {
+            let first = stack.pop()
+            let sec = stack.pop()
+            stack.push(Math.trunc(sec / first))
         }else {
-            st.push(c);
+            stack.push(Number(i));
         }
     }
-
-    return st[0];
-};
+    
+    return stack[0];
+}
