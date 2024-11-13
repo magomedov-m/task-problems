7eb0af8bdf3d627179d7d6d2e1aef88518d12391
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2024-11-13 23:58:20 +0300

    Time: 0 ms (100.00%) | Memory: 49.3 MB (37.49%) - LeetSync

--- 35-search-insert-position/search-insert-position.js
+++ 35-search-insert-position/search-insert-position.js
@@ -14,13 +14,20 @@ var searchInsert = function(nums, target) {
     //     else if (target > nums[nums.length - 1]) return nums.length
     // }
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == target) {
-            return i;
-        }else if (nums[i] > target) {
-            return i;
+    let start = 0;
+    let end = nums.length - 1;
+
+    while (start <= end) {
+        let middle = Math.floor((end + start) / 2);
+
+        if (nums[middle] > target) {
+            end = middle - 1;
+        }else if (nums[middle] < target) {
+            start = middle + 1;
+        }else {
+            return middle;
         }
     }
 
-    return nums.length;
+    return start;
 };
