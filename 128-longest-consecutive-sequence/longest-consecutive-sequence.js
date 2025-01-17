dc1f3e5f35991238493f1e51891aebeeeff70fbb
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-01-17 22:21:25 +0300

    Time: 27 ms (98.41%) | Memory: 72.8 MB (45.31%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -3,20 +3,24 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    let sortedNums = nums.sort((a, b) => a - b);
-    let curr = 1;
-    let max = 0;
+    if (nums == null || nums.length === 0) return 0;
+  
+  const set = new Set(nums);
+  let max = 0;
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == nums[i + 1]) continue;
-        
-        if (nums[i + 1] === nums[i] + 1) {
-            curr++;
-        }else {
-            max = Math.max(curr, max);
-            curr = 1;
-        }
+  for (let num of set) {
+    if (set.has(num - 1)) continue;  // make sure starting from the beginning of sequence
+
+    let currNum = num;
+    let currMax = 1;
+
+    while (set.has(currNum + 1)) {
+      currNum++;
+      currMax++;
     }
+    max = Math.max(max, currMax);
+  }
 
-    return max;
+  return max;
+
 };
