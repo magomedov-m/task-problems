40ed385229c450641e7272e38cbeb901f0550a5c
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-03-16 23:16:39 +0300

    Time: 2 ms (89.11%) | Memory: 58.3 MB (23.31%) - LeetSync

---
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+
+    let l = 0;
+    let sum = 0;
+    let minLen = Infinity;
+
+    for (let r = 0; r < nums.length; r++) {
+        sum += nums[r];
+
+        while (sum >= target) {
+            minLen = Math.min(minLen, r - l + 1);
+            sum -= nums[l];
+            l++;
+        }
+    }
+
+    return minLen === Infinity ? 0 : minLen;
+};
