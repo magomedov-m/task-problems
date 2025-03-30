24b40f4998486f574474f1ca77dbdf87279ea717
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-03-30 22:26:19 +0300

    Time: 1 ms (93.06%) | Memory: 56 MB (52.02%) - LeetSync

--- 3644-minimum-positive-sum-subarray/minimum-positive-sum-subarray.js
+++ 3644-minimum-positive-sum-subarray/minimum-positive-sum-subarray.js
@@ -5,22 +5,22 @@
  * @return {number}
  */
 var minimumSumSubarray = function(nums, l, r) {
-    let n = nums.length;
+
     let minSum = Infinity;
-    
-    // Перебираем все возможные начальные позиции подмассива
-    for (let i = 0; i < n; i++) {
-        let sum = 0;
-        // Формируем подмассивы, заканчивающиеся на j, при условии, что длина не превышает r
-        for (let j = i; j < n && (j - i + 1) <= r; j++) {
-            sum += nums[j];
-            // Если текущая длина подмассива не меньше l и сумма > 0, обновляем minSum
-            if (j - i + 1 >= l && sum > 0) {
-                minSum = Math.min(minSum, sum);
+
+    for (let i = 0; i < nums.length; i++) {
+        let cur = 0;
+
+        for (let j = i; (j - i) < r && j < nums.length; j++) {
+            cur += nums[j];
+
+            if (j - i + 1 >= l && cur > 0) {
+                minSum = Math.min(minSum, cur);
             }
+
         }
     }
-    
-    return minSum === Infinity ? -1 : minSum;
+
+    return minSum == Infinity ? -1 : minSum;
 };
 
