92a2e109c5ff43cf9181160b4bb095ebe1f30c70
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-04-03 11:11:45 +0300

    Time: 0 ms (100.00%) | Memory: 53.8 MB (32.46%) - LeetSync

---
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let l = 0;
+    let r = nums.length - 1;
+
+    while (l < r) {
+        let mid = Math.floor((l + r) / 2);
+
+        if (nums[mid] < nums[r]) {
+            r = mid;
+        }else {
+            l = mid + 1;
+        }
+    }
+
+    return nums[l];
+};
