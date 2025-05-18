51ed6e7cf5d4953b0510fd8db52593379b5fd84b
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-05-18 14:04:19 +0300

    Time: 15 ms (43.00%) | Memory: 65.1 MB (5.04%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -5,21 +5,22 @@
  */
 var topKFrequent = function(nums, k) {
     let mp = new Map();
-    let request = [];
+    let arr = new Array(nums.length + 1).fill().map(() => []);
+    let resArr = [];
 
     for (let i of nums) {
-        if (mp.has(i)) {
-            mp.set(i, mp.get(i) + 1);
-        }else {
-            mp.set(i, 1);
+        mp.set(i, mp.get(i) + 1 || 1);
+    }
+
+    for (let [i, j] of mp) {
+        arr[j].push(i);
+    }
+
+    for (let i = arr.length - 1; i >= 0 && resArr.length < k; i--) {
+        if (arr[i]) {
+            resArr.push(...arr[i]);
         }
     }
 
-    let sortMap = [...mp.entries()].sort((a, b) => b[1] - a[1]);
-
-    for (let i = 0; i < k; i++) {
-        request.push(sortMap[i][0]);
-    }
-
-    return request;
-};
+    return resArr.slice(0, k);
+}; 
