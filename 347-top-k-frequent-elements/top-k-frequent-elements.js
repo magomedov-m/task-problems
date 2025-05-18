c928182e3e0c027eef3d3681442a70d08e3a85e6
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-05-18 14:56:23 +0300

    Time: 20 ms (22.72%) | Memory: 65.7 MB (5.04%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -4,23 +4,24 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
+    // решена не полностью мной, (18.05.2025г.)
     let mp = new Map();
     let arr = new Array(nums.length + 1).fill().map(() => []);
-    let resArr = [];
+    let res = [];
 
     for (let i of nums) {
-        mp.set(i, mp.get(i) + 1 || 1);
+        mp.set(i, mp.get(i) + 1 || 0);
     }
 
     for (let [i, j] of mp) {
         arr[j].push(i);
     }
 
-    for (let i = arr.length - 1; i >= 0 && resArr.length < k; i--) {
+    for (let i = nums.length - 1; i >= 0 && res.length < k; i--) {
         if (arr[i]) {
-            resArr.push(...arr[i]);
+            res.push(...arr[i])
         }
     }
 
-    return resArr.slice(0, k);
+    return res;
 }; 
