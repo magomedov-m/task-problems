9120427fa4fa5e156b9f3f7d1265f711d1e4e737
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-05-20 12:06:36 +0300

    Time: 33 ms (46.29%) | Memory: 86.1 MB (7.77%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.js
+++ 219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -4,17 +4,15 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
-    let set = new Set();
+    let mp = new Map();
 
-    for (let l = 0; l < nums.length; l++) {
-        if (set.has(nums[l])) {
-            return true;
-        }
-        set.add(nums[l]);
-        if (set.size > k) {
-            set.delete(nums[l - k]);
-        }
+    for (let i = 0; i < nums.length; i++) {
         
+        let val = Math.abs(mp.get(nums[i]) - i);
+
+        if (mp.has(nums[i]) && val <= k) return true;
+
+        mp.set(nums[i], i);
     }
 
     return false;
