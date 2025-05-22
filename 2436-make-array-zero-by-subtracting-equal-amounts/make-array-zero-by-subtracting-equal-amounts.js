4443d30eb5e60701bf46ef12cb9ed0f859c406c1
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-05-23 00:58:50 +0300

    Time: 0 ms (100.00%) | Memory: 52.4 MB (97.87%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.js
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.js
@@ -3,21 +3,7 @@
  * @return {number}
  */
 var minimumOperations = function(nums) {
-    let operations = 0;
+    let set = new Set(nums.filter((e) => e > 0));
 
-    while (true) {
-        let minElem = Math.min(...nums.filter(num => num > 0));
-
-        if (minElem === Infinity) break;
-
-        for (let i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[i] -= minElem;
-            }
-        }
-
-        operations++;
-    }
-
-    return operations;
+    return set.size;
 };
