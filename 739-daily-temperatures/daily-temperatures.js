71a38cf1671f23dfe4215d97ba9a169f782f82c3
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-05-22 18:20:58 +0300

    Time: 13 ms (96.18%) | Memory: 80.9 MB (29.60%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -2,19 +2,19 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function(t) {
-    let n = t.length;
-    let res = new Array(n).fill(0);
-    let stack = [];
+var dailyTemperatures = function(temperatures) {
+    // задача не решена самостоятельно, подсказка: использовать монотонные стек. решить эту задачу 29.05
+     const n = temperatures.length;
+    const ans = new Array(n).fill(0);
+    const st = [];
 
-    for (let i = 0; i < n; i++) {
-        while (stack.length > 0 && t[i] > t[stack[stack.length - 1]]) {
-            let index = stack.pop();
-            res[index] = i - index;
+    for (let i = n - 1; i >= 0; i--) {
+        while (st.length && temperatures[i] >= temperatures[st[st.length - 1]]) {
+            st.pop();
         }
-
-        stack.push(i);
+        ans[i] = st.length ? st[st.length - 1] - i : 0;
+        st.push(i);
     }
-
-    return res;
+    
+    return ans;
 };
