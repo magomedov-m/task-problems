1283784097c2cfaa1a9c84fd823980607e91d9cf
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-06-04 19:07:15 +0300

    Time: 5 ms (78.30%) | Memory: 71.1 MB (48.29%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -3,26 +3,19 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let n = nums.length;
-    let arr1 = new Array(n);
-    let arr2 = new Array(n);
-
-    for (let i = 1; i < nums.length; i++) {
-        arr1[0] = nums[0];
-        arr1[i] = nums[i] * arr1[i - 1];
+let res = new Array(nums.length).fill(1);
+    
+    let prefix = 1;
+    for (let i = 0; i < nums.length; i++) {
+        res[i] = prefix;
+        prefix *= nums[i];
     }
-    for (let i = n - 2; i >= 0; i--) {
-        arr2[n - 1] = nums[n - 1]
-        arr2[i] = nums[i] * arr2[i + 1];
+    
+    let postfix = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        res[i] *= postfix;
+        postfix *= nums[i];
     }
-
-    let res = new Array(n);
-    res[0] = arr2[1];
-    res[n - 1] = arr1[n - 2];
-
-    for (let i = 1; i < n - 1; i++) {
-        res[i] = arr1[i - 1] * arr2[i + 1]
-    }
-
+    
     return res;
 };
