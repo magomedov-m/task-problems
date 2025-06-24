8a4400d45aa2dac4ff727e25b3d6568175cd4393
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-06-24 20:22:58 +0300

    Time: 0 ms (100.00%) | Memory: 57.1 MB (88.82%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -3,21 +3,48 @@
  * @param {number} target
  * @return {number}
  */
+
+/*
+Q:
+ 1. nums.length = 0 - false
+ 2. item - -10^4 / 10^4
+
+A: 
+bin search
+t = 4
+1 2 3 4 6 7
+      ^
+      ^
+      m
+
+Time: O(log n)
+Space: O(1)    
+*/
 var search = function(nums, target) {
-    let start = 0;
-    let end = nums.length - 1;
+    let l = 0;
+    let r = nums.length - 1;
+    
+    while (l <= r) {
+        let mid = Math.floor(l + (r - l) / 2);
 
-    while (start <= end) {
-        let mid = Math.floor((start + end) / 2);
-
-        if (nums[mid] < target) {
-            start = mid + 1;
+        if (nums[mid] == target) {
+            return mid;
         }else if (nums[mid] > target) {
-            end = mid - 1;
-        }else if (nums[mid] === target) {
-            return mid
+            r = mid - 1;
+        }else {
+            l = mid + 1;
         }
     }
 
     return -1;
 };
+
+/*
+test:
+1.
+ t = 1 
+ 1 3 5 6
+ ^
+ ^
+ m
+*/
