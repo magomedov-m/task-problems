cabab2c708f8403ba6d0ba198ebddcb70ed72db1
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-06-27 09:41:37 +0300

    Time: 6 ms (12.81%) | Memory: 60.4 MB (63.18%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.js
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.js
@@ -4,27 +4,26 @@
  * @return {number}
  */
 var longestOnes = function(nums, k) {
-    if (nums.length == 1) return 1;
-
-    let countZeroes = 0;
-    let l = 0;
     let maxLen = 0;
+    let l = 0;
+    let r = 0;
+    let count = 0;
 
-    for (let r = 0; r < nums.length; r++) {
-
-        if (nums[r] === 0) {
-            countZeroes++;
-        }
-
-        while (countZeroes > k) {
-            if (nums[l] === 0) {
-                countZeroes--;
+    while (r <= nums.length) {
+        while (count > k) {
+            if (nums[l] == 0) {
+                count--;
             }
             l++;
         }
 
+        if (nums[r] == 0) {
+            count++;
+        }
+
+        r++;
         maxLen = Math.max(maxLen, r - l + 1);
     }
 
-    return maxLen;
+    return maxLen - 2;
 };
