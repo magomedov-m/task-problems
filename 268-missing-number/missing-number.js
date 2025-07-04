bb8c2bd71e48908a858ea8eee4615c8fd042b5a4
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-07-04 14:26:57 +0300

    Time: 2 ms (48.36%) | Memory: 54.9 MB (81.95%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -2,14 +2,48 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function(nums) {
-    let n = nums.length;
-    let S = (n * (n + 1)) / 2;
-    let sum = 0;
 
-    for (let i = 0; i < nums.length; i++) {
-        sum += nums[i];
+/*
+Q:
+ 1. sorted - false
+ 2. valid number - true
+ 3. nums.length > 0 - true
+ 4. unique number - true
+
+A:
+ sum1 = 10
+ 0 1 2 3 4
+         ^
+ sum2 = 7
+ 0 1 2 4
+       ^
+
+Time: O(n);
+Space: O(1);
+*/
+var missingNumber = function(nums) {
+    let sum1 = 0;
+    let sum2 = 0;
+
+    for (let i = 0; i < nums.length + 1; i++) {
+        sum1 += i;
     }
 
-    return S - sum;
+    for (let i of nums) {
+        sum2 += i;
+    }
+
+    return sum1 - sum2;
 };
+
+/*
+test:
+
+1. 
+ sum1 = 6
+ i = 3
+ 0 1 2 3
+ sum2 = 5
+ 0 2 3
+     ^
+*/
