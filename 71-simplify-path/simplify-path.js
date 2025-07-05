149fa51817b7427b5cbabb04bf26f797f9821516
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-07-05 15:23:12 +0300

    Time: 4 ms (34.18%) | Memory: 56.5 MB (29.77%) - LeetSync

--- 71-simplify-path/simplify-path.js
+++ 71-simplify-path/simplify-path.js
@@ -3,25 +3,18 @@
  * @return {string}
  */
 var simplifyPath = function(path) {
-    if (path === '/') return '/';
-    let arr = path.split('/');
     let stack = [];
-    
-    for (let i of arr) {
-        if (i === '..') {
+    let separatePath = path.split('/');
+
+    for (let i of separatePath) {
+        if (i == '..') {
             stack.pop();
-        }else if (i === '.' || i === '') {
+        }else if (i == '.' || i == '') {
             continue;
         }else {
             stack.push(i);
         }
     }
-    if (stack.length == 0) return '/';
-    let res = '';
-    for (let i = 0; i < stack.length; i++) {
-        res += '/';
-        res += stack[i];
-    }
-    
-    return res;
+
+    return '/' + stack.join('/');
 };
