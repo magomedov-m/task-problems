520bc2d4aafdc5d0f8f7ea29ba97b72991dc29e4
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-11-06 13:41:10 +0300

    Time: 2 ms (8.86%) | Memory: 55.3 MB (42.28%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.js
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.js
@@ -2,18 +2,34 @@
  * @param {string} s
  * @return {number}
  */
+// [, { - false
+// ((null)) - true
+// (() - false
+// 1 <= s.length <= 100
+// letters - false
+// stack
+
+// val = 2
+// []
+// () + (())
+//         ^
 var maxDepth = function(s) {
     let stack = [];
-    let value = 0;
+    let val = 0;
 
     for (let i of s) {
-        if (i === '(') {
-            stack.push(i);
-        }else if (i === ')') {
+        if (i == '(') {
+            stack.push('(');
+        }else if (i == ')') {
+            val = Math.max(val, stack.length);
             stack.pop();
         }
-        value = Math.max(value, stack.length);
     }
 
-    return value;
+    return val;
 };
+
+// val = 3
+// []
+// (1+(2*3)+((8)/4))+1
+//                 ^
