abe0111311ae0a13e7c9cd18ccf12233263a26b4
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-11-11 14:26:29 +0300

    Time: 49 ms (34.01%) | Memory: 55.2 MB (8.38%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.js
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.js
@@ -11,20 +11,30 @@
  * @param {number} n
  * @return {number}
  */
+ // [] - false
+ // pick in arr - true
+ // num > 0
 var guessNumber = function(n) {
-    let start = 0; 
-    let end = n - 1;
+    let l = 0;
+    let r = n;
 
-    while (start <= end) {
-        let mid = Math.floor((start + end) / 2);
-        let res = guess(mid);
-        if (res == 0) {
+    while (l <= r) {
+        let mid = Math.floor((r - l) / 2) + l;
+
+        let value = guess(mid);
+
+        if (value == 0) {
             return mid;
-        }else if (res == -1) {
-            end = mid - 1;
-        }else if (res == 1) {
-            start = mid + 1;
+        }else if (value == -1) {
+            r = mid - 1;
+        }else {
+            l = mid + 1;
         }
     }
-    return start
 };
+
+// 10, 6
+// mid = 6
+// value = 0
+// l = 6
+// r = 7
