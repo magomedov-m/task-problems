38005155e0ad92e409783b06715dff595b574f12
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-11-11 10:13:56 +0300

    Time: 6 ms (33.89%) | Memory: 61.1 MB (5.00%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -2,48 +2,32 @@
  * @param {number[]} nums
  * @return {number}
  */
-
-/*
-Q:
- 1. sorted - false
- 2. valid number - true
- 3. nums.length > 0 - true
- 4. unique number - true
-
-A:
- sum1 = 10
- 0 1 2 3 4
-         ^
- sum2 = 7
- 0 1 2 4
-       ^
-
-Time: O(n);
-Space: O(1);
-*/
 var missingNumber = function(nums) {
-    let sum1 = 0;
-    let sum2 = 0;
-
-    for (let i = 0; i < nums.length + 1; i++) {
-        sum1 += i;
-    }
+    let mp = new Map();
 
     for (let i of nums) {
-        sum2 += i;
+        mp.set(i, true);
     }
 
-    return sum1 - sum2;
+    for (let i = 0; i <= nums.length; i++) {
+        if (!mp.has(i)) {
+            return i;
+        }
+    }
 };
 
-/*
-test:
 
-1. 
- sum1 = 6
- i = 3
- 0 1 2 3
- sum2 = 5
- 0 2 3
-     ^
-*/
+// [3-true, 0-true, 1-true]
+// [3, 0, 1]
+//        ^
+// i = 2
+
+// [1-true]
+// [1]
+//  ^
+// i = 0
+
+// [0-true]
+// [0]
+//    ^
+// i = 1
