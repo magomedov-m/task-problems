834cf8cbeba5f8583eee6d117610148ec7301917
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-11-27 13:45:33 +0300

    Time: 15 ms (19.71%) | Memory: 65.3 MB (5.11%) - LeetSync

--- 4011-smallest-absent-positive-greater-than-average/smallest-absent-positive-greater-than-average.js
+++ 4011-smallest-absent-positive-greater-than-average/smallest-absent-positive-greater-than-average.js
@@ -16,17 +16,11 @@ var smallestAbsent = function(nums) {
     let midValue = sum / len;
     let startIdx = Math.ceil(midValue);
 
-    // while (mp.has(startIdx) || startIdx == midValue || startIdx <= 0) {
-    //     startIdx++;
-    // }
-    for (startIdx; startIdx < Infinity; startIdx++) {
-        if (mp.has(startIdx) || startIdx == midValue || startIdx <= 0) {
-            continue;
-        }else {
-            return startIdx;
-        }
+    while (mp.has(startIdx) || startIdx == midValue || startIdx <= 0) {
+        startIdx++;
     }
-    
+
+    return startIdx;
 };
 // mp = 3:true; 5:true;
 // sum = 8
