f8c1471913761c4ba5832b92946a6eea97c4a2ee
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-11-29 13:53:00 +0300

    Time: 0 ms (100.00%) | Memory: 57.6 MB (62.77%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.js
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.js
@@ -3,19 +3,39 @@
  * @return {number[]}
  */
 var finalPrices = function(prices) {
-    let stack = null;
-    let answer = [];
+    if (prices.length == 1) return prices;
+    let res = [];
 
-    for (let i = 0; i < prices.length; i++) {
-        stack = prices[i];
-        for (let j = i + 1; j < prices.length; j++) {
-            if (prices[i] - prices[j] >= 0) {
-                stack = prices[i] - prices[j]
+    for (let i = 0; i < prices.length - 1; i++) {
+        let j = i + 1;
+        for (j; j < prices.length; j++) {
+            
+            if (prices[j] <= prices[i]) {
+                let value = prices[i] - prices[j];
+                res.push(value);
                 break;
-            } 
+            }else if (j == prices.length - 1) {
+                res.push(prices[i]);
+            }
         }
-        answer.push(stack)
     }
 
-    return answer;
+    res.push(prices[prices.length - 1]);
+
+    return res;
 };
+
+// 4 2 4 2
+// 8 4 6 2 3
+//       i
+//         j
+
+// 1 2 3 4 5
+// 1 2 3 4 5
+//       i
+//         j
+
+// 9 0 1 6
+// 10 1 1 6
+//      i
+//        j
