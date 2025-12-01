4d547e3718a075331bef114e2ea68a2b64bec9e8
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-12-01 16:47:22 +0300

    Time: 4 ms (13.13%) | Memory: 55.9 MB (79.29%) - LeetSync

--- 3447-clear-digits/clear-digits.js
+++ 3447-clear-digits/clear-digits.js
@@ -2,15 +2,24 @@
  * @param {string} s
  * @return {string}
  */
+ // 4bc3n -> 4bn
+ // '' - false
+ // 4 - ''
+ // ba
 var clearDigits = function(s) {
     let stack = [];
 
+    function isDigit(n) {
+        return n.toLowerCase() == n.toUpperCase();
+    }
+
     for (let i of s) {
-        if (!isNaN(i)) {
-            stack.pop();
-        }else {
-            stack.push(i);
+        if (isDigit(i)) {
+            if (stack.length > 0) stack.pop();
+            continue;
         }
+
+        stack.push(i);
     }
 
     return stack.join('');
