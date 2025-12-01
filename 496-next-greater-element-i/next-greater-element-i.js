9ee55aa08cde5a7746452413e2b024a0ae5afb37
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-12-01 14:32:16 +0300

    Time: 2 ms (85.73%) | Memory: 56.1 MB (88.93%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -4,15 +4,29 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    let res = [];
+    let stack = [];
+    let mp = new Map();
 
-    for (let i of nums1) {
-        let x = -1
-        for (let j = nums2.length - 1; nums2[j] != i; j--) {
-            if (nums2[j] > i) x = nums2[j]
+    for (let i of nums2) {
+        while (stack.length > 0 && i > stack[stack.length - 1]) {
+            mp.set(stack.pop(), i);
         }
-        res.push(x)
+
+        stack.push(i);
     }
 
-    return res;
+    for (let i of stack) {
+        mp.set(i, -1);
+    }
+
+    for (let i = 0; i < nums1.length; i++) {
+        nums1[i] = mp.get(nums1[i]);
+    }
+
+    return nums1;
 };
+// 0: 1; 1: 3;
+// 4 1 2 - 1 3 4 2
+//               ^
+// stack = 4 2
+// mp = 1: 3, 3: 4, 
