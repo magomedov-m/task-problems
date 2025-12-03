5462bd0e78195cef283cc9068fcaab52649fad7c
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-12-03 23:02:55 +0300

    Time: 0 ms (100.00%) | Memory: 54.9 MB (17.47%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -3,15 +3,36 @@
  * @param {number} val
  * @return {number}
  */
+//  nums = [] - false
+//  val !includes nums - false
+//  resSort - false
+
 var removeElement = function(nums, val) {
     let l = 0;
+    let r = nums.length - 1;
 
-    for (let r = 0; r < nums.length; r++) {
-        if (nums[r] !== val) {
-            nums[l] = nums[r];
-            l++;
+    while (l <= r) {
+        while (nums[r] == val) r--;
+
+        if (nums[l] == val) {
+            [nums[l], nums[r]] = [nums[r], nums[l]];
+            l--;
         }
+
+        l++;
     }
 
     return l;
 };
+
+// 2
+// ^
+// ^
+
+// 2 2 3 3
+//   ^
+//   ^
+
+// 1 2 4 3 3
+//     ^
+//     ^
