04d143daaa0755412634742b6431bf57546068ee
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-12-07 22:03:59 +0300

    Time: 40 ms (37.76%) | Memory: 70.9 MB (95.67%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -2,57 +2,33 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-/*
-Q:
- 1. 3 <= nums.length <= 3000
- 2. -10^5 <= nums[i] <= 10^5
- 3. 
-
-A:
- -1 1 0
-  ^
-    ^
-      ^
-Time: O(n^3);
-Space: O(1)
-*/
 var threeSum = function(nums) {
     let res = [];
     nums.sort((a, b) => a - b);
-
+    
     for (let i = 0; i < nums.length - 2; i++) {
         let l = i + 1;
         let r = nums.length - 1;
-
-        if (i > 0 && nums[i] == nums[i - 1]) {
-            continue;
-        }
-
+        
         while (l < r) {
-            let curVal = nums[l] + nums[r] + nums[i];
-
-            if (curVal == 0) {
+            
+            let sum = nums[i] + nums[l] + nums[r];
+            
+            if (sum == 0) {
                 res.push([nums[i], nums[l], nums[r]]);
+                while (nums[l] == nums[l + 1]) l++;
+                while (nums[r] == nums[r - 1]) r--;
                 l++;
                 r--;
-
-                while (nums[l] == nums[l - 1]) l++;
-                while (nums[r] == nums[r + 1]) r--;
-            }else if (curVal > 0) {
+            }else if (sum > 0) {
                 r--;
             }else {
                 l++;
             }
         }
+        
+        while (nums[i] == nums[i + 1]) i++;
     }
-
+    
     return res;
 };
-/*
- [[1, 0, -1], [-1, 2, -1], []]
- -1 0 1 2 -1 -4
- -4 -1 -1 0 1 2
-    i
-      j
-           c
-*/
