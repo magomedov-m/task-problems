cbbfc1cc8b2f2846e24cb318af25cfc0d5506cfb
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-12-07 13:36:57 +0300

    Time: 3 ms (52.51%) | Memory: 63.6 MB (47.30%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -2,59 +2,23 @@
  * @param {number[]} height
  * @return {number}
  */
-
-/*
-Q:
- 1. h.length < 3 - false
- 2. every elem = 0 - true
- 3. valid number - true
-
-A:
- maxValue = 
- 1 8 6 2 5 4 8 3 7
-   ^
-     ^
-
-Time: O(n)
-Space: O(1)
-*/
-
-var maxArea = function(h) {
-    let maxVal = 0;
-
+var maxArea = function(height) {
+    let n = height.length;
+    let res = 0; // 10
     let l = 0;
-    let r = h.length - 1;
-
+    let r = n - 1;
+    
     while (l < r) {
-        let minElem = Math.min(h[r], h[l]);
-        let cur = (r - l) * minElem
-        maxVal = Math.max(cur, maxVal)
-
-        if (h[l] <= h[r]) {
+        let val = Math.min(height[l], height[r]) * (r - l);
+        
+        res = Math.max(res, val);
+        
+        if (height[l] <= height[r]) {
             l++;
         }else {
             r--;
         }
     }
-
-    return maxVal;
+    
+    return res;
 };
-
-/*
-test:
- 0 0 0 0
-       ^
-       ^
-
- 1 2 3 4
-     ^
-       ^
-
- 0 0
-   ^
-   ^
-
- 9 9 9 9
-       ^
-       ^
-*/
