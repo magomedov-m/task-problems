f23e524a72dc49da40f1b690293c24a9c3f071ab
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-12-08 01:03:00 +0300

    Time: 6 ms (68.68%) | Memory: 58.5 MB (42.87%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -2,56 +2,27 @@
  * @param {string} s
  * @return {number}
  */
-/*
-Q:
- 1. 0 <= str.length <= 5 * 10^4
- 2. str consist eng lowerCase char, number, " "
- 3. valid number - true
-
-A:
- mp = []
- 'abca'
-   ^
-     ^
-
-Time: O(n)
-Space: O(n)
-*/
 var lengthOfLongestSubstring = function(s) {
-    if (s.length < 2) return s.length;
-
-    let mp = new Map();
+    if (s.length < 1) return s.length;
+    
+    let resLen = 1;
+    let hs = new Set();
     let l = 0;
-    let r = 0;
-    let maxLen = 0;
-
-    while (r < s.length) {
-        while (mp.has(s[r])) {
-            mp.delete(s[l]);
+    
+    for (let r = 0; r < s.length; r++) {
+        while (hs.has(s[r])) {
+            hs.delete(s[l]);
             l++;
         }
-
-        maxLen = Math.max(maxLen, r - l);
-        mp.set(s[r]);
-        r++;
+    
+        hs.add(s[r]);
+        resLen = Math.max(r - l + 1, resLen);
     }
-
-    return maxLen + 1;
+    
+    return resLen;
 };
-
-/*
-test:
-1.
- abca
-  ^
-    ^
-
-2.
- ''
-
-3.
-maxLen = 2;
- abcabcbb
-       ^
-        ^
-*/
+// abcabcbb
+// l
+//   r
+// res = 
+// set = 
