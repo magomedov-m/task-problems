16046854dd38428e06d6c400ddc06c2a2de8d0e3
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-12-07 20:20:41 +0300

    Time: 2 ms (61.82%) | Memory: 55.4 MB (61.34%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -3,28 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
-/*
-Q:
- 1. numbers.length >= 2
- 2. numbers[i] -1000 > 1000
- 3. sorted - true
- 4. every elem not valid - true
-
-A:
- 1 4 4 5
- ^
-     ^
-
-Time: O(n);
-Space: O(1)
-*/
 var twoSum = function(numbers, target) {
-
     let l = 0;
     let r = numbers.length - 1;
 
+    let res = [];
+
     while (l < r) {
         let val = numbers[l] + numbers[r];
+
         if (val == target) {
             return [l + 1, r + 1];
         }else if (val < target) {
