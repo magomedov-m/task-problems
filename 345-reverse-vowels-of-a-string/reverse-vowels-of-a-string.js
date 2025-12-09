95df56e713185345f3c27a961dd86776a1112d19
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-12-09 21:58:45 +0300

    Time: 12 ms (25.40%) | Memory: 62.2 MB (24.28%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    let arrS = s.split('');
+    
+    let obj = {
+        'a': true,
+        'e': true,
+        'i': true,
+        'o': true,
+        'u': true,
+    }
+    
+    let l = 0;
+    let r = s.length - 1;
+    
+    function isVowel(letter) {
+        if (obj[letter.toLowerCase()]) return true;
+        
+        return false;
+    }
+    
+    while (l < r) {
+        while (l < r && !isVowel(arrS[l])) l++;
+        while (r > l && !isVowel(arrS[r])) r--;
+        
+        if (l < r) {
+            [arrS[l], arrS[r]] = [arrS[r], arrS[l]];
+            l++;
+            r--;
+        }
+    }
+    
+    return arrS.join('');
+};
