39825850622055ebd276d8c93f30f8477c41c4c4
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-12-15 17:02:06 +0300

    Time: 0 ms (100.00%) | Memory: 53.7 MB (74.24%) - LeetSync

--- 228-summary-ranges/summary-ranges.js
+++ 228-summary-ranges/summary-ranges.js
@@ -2,63 +2,38 @@
  * @param {number[]} nums
  * @return {string[]}
  */
-
-/*
-Q:
- 1. [] - true
- 2. valid number - false
- 3. sorted - true
- 4. duplicates - false
-
-A:
-two pointers
-
-res = []
-str = ''
- 1 2 4 5 7
-         ^
-         ^
-
-Time: O(n + k)
-Space: O(1)
-*/
 var summaryRanges = function(nums) {
-    if (!nums.length) return [];
-
+    if (nums.length < 1) return [];
+    
     let res = [];
-    let str = '';
     let l = 0;
     let r = 0;
 
-    while (r < nums.length) {
-
-        if (r == nums.length - 1 || nums[r + 1] - nums[r] > 1) {
-            if (nums[r] != nums[l]) {
-                str += `${nums[l]}->${nums[r]}`
-                res.push(str);
-                str = '';
-
+    for (r; r < nums.length - 1; r++) {
+        if (nums[r + 1] - nums[r] > 1) {
+            if (nums[r] === nums[l]) {
+                res.push(`${nums[r]}`);
                 l = r + 1;
             }else {
-                str += `${nums[l]}`
-                res.push(str);
-                str = ''
-                l++
+                res.push(`${nums[l]}->${nums[r]}`);
+                l = r + 1;
             }
         }
-        r++;
     }
 
-    return res;
+    while (r < nums.length) {
+
+        if (nums[l] === nums[r]) {
+            res.push(`${nums[r]}`);
+        }else {
+            res.push(`${nums[l]}->${nums[r]}`)
+        }
+        r++
+    }
+
+    return res
 };
 
-/*
-str = ''
-res = ['0->2', '4->5', '', ]
-r = 0
-l = 0
-
- 0 1 2 4 5 7
-           ^
-           ^
-*/
+// 1 2 4 5 8 10 11 12 13
+//             l
+//                  r
