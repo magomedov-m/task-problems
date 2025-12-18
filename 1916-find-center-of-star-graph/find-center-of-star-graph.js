6750e647ee1500cd75c03c0000e8eaa221396f7a
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-12-19 00:27:39 +0300

    Time: 0 ms (100.00%) | Memory: 76.6 MB (99.18%) - LeetSync

---
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var findCenter = function(edges) {
+    let visited = new Set();
+    let queu = [];
+
+    for (let i = 0; i < edges.length; i++) {
+        queu.push(edges[i]);
+
+        if (visited.has(edges[i][0])) {
+            return edges[i][0];
+        }else if (visited.has(edges[i][1])) {
+            return edges[i][1];
+        }else {
+            visited.add(edges[i][0]);
+            visited.add(edges[i][1]);
+        }
+    }
+};
