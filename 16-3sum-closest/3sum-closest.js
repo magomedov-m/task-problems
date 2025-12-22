d7f9ac42f49a580d7782975242a48194d4469f84
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-12-22 23:37:30 +0300

    Time: 12 ms (83.92%) | Memory: 57.6 MB (38.65%) - LeetSync

--- 16-3sum-closest/3sum-closest.js
+++ 16-3sum-closest/3sum-closest.js
@@ -3,79 +3,34 @@
  * @param {number} target
  * @return {number}
  */
-
-/*
-Q:
- 1. 3 <= nums.length <= 500
- 2. item +-1000
- 3. valid number - false
- 4. sorted - false
- 5. sum items = target
-
-A:
- t = 3
- minVal = 1
-
- -1 -2 2 4 3
- -2 -1 2 3 1
-       i
-         ^ 
-           ^
-
-Time: O(n * k);
-Space: O(1);
-*/
 var threeSumClosest = function(nums, target) {
+    let closest = Infinity;
+    let sum = 0;
+
     nums.sort((a, b) => a - b);
-    let minVal = Infinity;
-    let res = 0;
 
     for (let i = 0; i < nums.length - 2; i++) {
         let l = i + 1;
         let r = nums.length - 1;
 
         while (l < r) {
-            let cur = Math.abs(nums[l] + nums[i] + nums[r] - target);
-            let val = nums[l] + nums[i] + nums[r];
+            let curSum = nums[i] + nums[l] + nums[r];
+            let val = Math.abs(curSum - target);
 
-            if (cur < minVal) {
-                minVal = Math.abs(cur);
-                res = val;
+            if (val < closest) {
+                closest = val;
+                sum = curSum;
             }
-            if (val > target) {
+            
+            if (curSum > target) {
                 r--;
-            }else {
+            }else if (curSum < target) {
                 l++;
+            }else {
+                return target;
             }
         }
     }
 
-    return res;
+    return sum;
 };
-
-/*
-test:
- 1. 
- t = 1
- minVal = 1
- -2 1 2
- ^
-    ^
-      ^
- 
- 2.
- t = 5
- minVal = 0
- 0 0 0 0
- ^
-   ^
-       ^
-
- 3.
- t = 10
- minVal = -3
- -5 -2 1 2 4
-  ^
-     ^
-           ^
-*/
