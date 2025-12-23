4d795804370aae829b61cbd7bbdda72dd4801192
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-12-24 02:09:07 +0300

    Time: 13 ms (85.35%) | Memory: 57.1 MB (70.39%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
@@ -5,32 +5,26 @@
  */
 var characterReplacement = function(s, k) {
     let mp = new Map();
-
+    let res = 0;
     let l = 0;
-    let maxLen = 0;
-    let maxValue = -Infinity;
-
-    // for (let r = 0; r < s.length; r++) {
-    //     mp.set(s[r], (mp.get(s[r]) || 0) + 1);
-    //     minValue = Math.min(minValue, mp.get(s[r]));
-
-    //     while (minValue > k) {
-    //         mp.set(s[l], mp.get(s[l]) - 1);
-    //         l++;
-    //     }
-    //     maxLen = Math.max(res, r - l + 1);
-    // }
+    let maxf = 0;
 
     for (let r = 0; r < s.length; r++) {
-        mp.set(s[r], (mp.get(s[r]) || 0) + 1);
-        maxValue = Math.max(mp.get(s[r]), maxValue);
+        mp.set(s[r], (mp.get(s[r]) ?? 0) + 1);
+        maxf = Math.max(maxf, mp.get(s[r]));
 
-        while((r - l + 1) - maxValue > k) {
-            mp.set(s[l], mp.get(s[l]) - 1);
+        while (r - l + 1 - maxf > k) {
+            if (mp.get(s[l]) === 1) {
+                mp.delete(s[l]);
+            }else {
+                mp.set(s[l], mp.get(s[l]) - 1);
+            }
+
             l++;
         }
-        maxLen = Math.max(maxLen, r - l + 1);
+
+        res = Math.max(res, r - l + 1);
     }
 
-    return maxLen;
+    return res;
 };
