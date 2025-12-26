d7e93fd7b47100bbe985ebd9e4ab0e391a4d2e76
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-12-26 17:32:08 +0300

    Time: 0 ms (100.00%) | Memory: 54.3 MB (95.67%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
@@ -11,32 +11,28 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-/*
-Q:
-  1. len == 1 - true
-  2. len == 0 - false
-  3. n <= len
-  4. only number - true
-*/
+    let dummy = new ListNode(0, head);
+    let cur = dummy;
+    let temp = dummy;
 
-    let res = new ListNode();
-    res.next = head;
-    let dummy = res;
-
-    let count = 0;
-    let current = head;
-    while (current) {
-        count++;
-        current = current.next;
+    let len = 0;
+    while (cur.next) {
+        len++;
+        cur = cur.next;
     }
 
-    
-    while (count > n) {
-        count--
-        dummy = dummy.next;
+    len -= n;
+
+    while (temp.next) {
+
+        if (len === 0) {
+            temp.next = temp.next.next;
+            break;
+        }
+        len--;
+        
+        temp = temp.next;
     }
 
-    dummy.next = dummy.next.next;
-
-    return res.next;
+    return dummy.next;
 };
