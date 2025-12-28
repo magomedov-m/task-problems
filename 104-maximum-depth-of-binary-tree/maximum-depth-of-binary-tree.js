b287df40e2d1a30356d99b18b7583ca2ec8ddef4
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-12-28 16:44:24 +0300

    Time: 0 ms (100.00%) | Memory: 56.8 MB (85.11%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    let max = 0;
+
+    function dfs(node) {
+        if (!node) return 0;
+
+        return Math.max(dfs(node.left), dfs(node.right)) + 1;
+    }
+
+    return dfs(root);
+};
