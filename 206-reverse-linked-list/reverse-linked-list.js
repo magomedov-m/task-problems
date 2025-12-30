d50f6b50db48149bcbd976638f37baeaf193f343
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-12-30 22:42:20 +0300

    Time: 0 ms (100.00%) | Memory: 56.2 MB (86.96%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.js
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -9,18 +9,50 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+// var reverseList = function(head) {
+//     if (!head) return head;
+//     let prev = null;
+//     let next = null;
+//     let newHead = null;
+    
+//     while (head) {
+//         next = head.next;
+//         head.next = prev;
+//         prev = head;
+//         newHead = next ? next : head;
+//         head = next;
+//     }
+    
+//     return newHead;
+// };
+
 var reverseList = function(head) {
-
-    let prev = null;
-    let curr = head;
-
-    while (curr != null) {
-        let next = curr.next;
-        curr.next = prev;
-
-        prev = curr;
-        curr = next;
+    if (!head) return head
+    let nodes = [];
+    
+    while (head) {
+        nodes.push(head);
+        head = head.next;
     }
+    
+    let newHead = nodes.pop();
+    while (nodes.length !== 0) {
+        let cur = nodes.pop();
+        newHead.next = cur;
+        newHead = cur;
+    }
+}
 
-    return prev;
-};
+var reverseList = function(head) {
+    let arr = [null];
+    while (head) {
+        arr.push(head);
+        head = head.next;
+    }
+    
+    for (let r = arr.length - 1; r >= 1; r--) {
+        arr[r].next = arr[r - 1];
+    }
+    
+    return arr[arr.length - 1];
+}
