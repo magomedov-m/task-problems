6c3ff648275eb952f5460a59749d35f5acbffbe6
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-12-31 11:49:08 +0300

    Time: 15 ms (9.94%) | Memory: 62.5 MB (5.59%) - LeetSync

--- 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.js
+++ 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.js
@@ -3,24 +3,43 @@
  * @return {number}
  */
 var maxDistToClosest = function(seats) {
-    let maxVal = 0;
-    let l = 0;
-    let r = 0;
-    
-    while (r < seats.length) {
-        if (seats[r] == 1 && seats[l] == 1) {
-            let curVal = Math.floor((r - l) / 2);
-            maxVal = Math.max(maxVal, curVal);
-            l = r;
-        }else if (r == seats.length - 1 || seats[r] == 1) {
-            if (seats[r] == 1 && seats[l] == 0 || seats[r] == 0 && seats[l] == 1) {
-                maxVal = Math.max(maxVal, r - l);
-                l = r;
-            }
+    let arr = new Array(seats.length).fill(Infinity);
+    let n = seats.length;
+    let lastPn = -1;
+    let max = 0;
+
+    for (let i = 0; i < n; i++) {
+        if (seats[i] === 1) {
+            lastPn = i;
         }
 
-        r++;
+        if (lastPn !== -1) {
+            arr[i] = i - lastPn;
+        }
     }
 
-    return maxVal;
+    lastPn = -1;
+
+    for (let j = n - 1; j >= 0; j--) {
+        if (seats[j] === 1) {
+            lastPn = j;
+        }
+
+        if (lastPn !== -1) {
+            arr[j] = Math.min(arr[j], lastPn - j)
+        }
+    }
+
+    for (let i of arr) {
+        max = Math.max(max, i);
+    }
+
+    return max;
 };
+// lastPn = 1
+
+// 0 1 -> seats
+//   j
+// 0 0 -> arr
+// 0 1 -> indexes
+
