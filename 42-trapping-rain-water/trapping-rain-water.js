13edd5685cdf4dff5a875a386d5cb16c0d695111
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2026-01-05 17:55:19 +0300

    Time: 8 ms (36.47%) | Memory: 59.9 MB (32.07%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.js
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -2,28 +2,48 @@
  * @param {number[]} height
  * @return {number}
  */
-var trap = function(h) {
-    if (h.length == 1) return 0;
-
-    let l = 0;
-    let r = h.length - 1;
-
-    let leftMax = h[l];
-    let rightMax = h[r];
-
+var trap = function(height) {
+    let len = height.length;
     let res = 0;
+    let prevMax = 0;
+    let nextMax = 0;
 
-    while (l < r) {
-        if (leftMax < rightMax) {
-            l++;
-            leftMax = Math.max(leftMax, h[l]);
-            res += leftMax - h[l];
-        }else {
-            r--;
-            rightMax = Math.max(rightMax, h[r]);
-            res += rightMax - h[r];
+    let leftArr = new Array(len).fill(0);
+    let rightArr = new Array(len).fill(0);
+
+    for (let left = 0; left < len; left++) {
+        let cur = height[left];
+
+        leftArr[left] = prevMax > cur ? prevMax : 0;
+
+        prevMax = Math.max(prevMax, cur);
+    }
+
+    for (let right = len - 1; right >= 0; right--) {
+        let cur = height[right];
+
+        rightArr[right] = nextMax > cur ? nextMax : 0;
+
+        nextMax = Math.max(nextMax, cur);
+    }
+
+    for (let el = 0; el < len; el++) {
+        let min = Math.min(leftArr[el], rightArr[el]);
+        let cur = height[el];
+
+        if (min > cur) {
+            res += min - cur;
         }
     }
 
     return res;
 };
+
+/*
+res = 9
+min = 4
+h 4 2 0 3 2 5
+          m
+l 0 4 4 4 4 0
+r 5 5 5 5 5 0
+*/
