c9052b7f308895221eb0dea5ad425e4a6e45f281
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2026-01-07 19:16:37 +0300

    Time: 28 ms (35.95%) | Memory: 78.9 MB (33.74%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -2,19 +2,43 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function(t) {
-    // задача не решена самостоятельно, подсказка: использовать монотонные стек. решить эту задачу 29.05
-    let res = new Array(t.length).fill(0);
+
+ /*
+ [] - false
+ 30 <= 30 <= 100
+ every duplicates
+
+[73, 74, 75, 71, 69, 72, 76, 73]
+                             ^
+[76, 73]
+ 1 1 1 2 1 4 0 0
+
+ T(n)
+ S(n)
+ */
+var dailyTemperatures = function(temperatures) {
+    let n = temperatures.length;
+
+    let res = new Array(n).fill(0);
     let stack = [];
 
-    for (let i = t.length - 1; i >= 0; i--) {
-        while (stack.length && t[i] >= t[stack[stack.length - 1]]) {
-            stack.pop()
+    for (let i = 0; i < n; i++) {
+        let curTmp = temperatures[i];
+
+        while (stack.length > 0 && curTmp > temperatures[stack[stack.length - 1]]) {
+            let prevIdx = stack.pop();
+            res[prevIdx] = i - prevIdx;
         }
 
-        res[i] = stack.length ? stack[stack.length - 1] - i : 0;
-        stack.push(i)
+        stack.push(i);
     }
 
     return res;
 };
+
+/*
+[1 2 1 0]
+[]
+30 55 40 90
+          i
+*/
