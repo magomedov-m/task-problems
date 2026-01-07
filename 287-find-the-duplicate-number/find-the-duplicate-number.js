9acfa91e9b6834b5c0a666fa2325cc82da396643
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2026-01-07 15:52:02 +0300

    Time: 21 ms (34.41%) | Memory: 76.3 MB (24.37%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -3,28 +3,13 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-/*
-Q:
- 1. nums.len < 2 - false
- 2. valid numbers - true
- 3. only number - true
- 4. sorted - false
- 5. every elem equal - true
-*/
+    let set = new Set()
 
-    let slow = nums[0];
-    let fast = nums[0];
+    for (let i of nums) {
+        if (set.has(i)) {
+            return i;
+        }
 
-    do {
-        slow = nums[slow];
-        fast = nums[nums[fast]]
-    }while (slow != fast);
-
-    slow = nums[0];
-    while (fast != slow) {
-        slow = nums[slow];
-        fast = nums[fast]
+        set.add(i);
     }
-
-    return slow;
 };
