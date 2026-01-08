813c02346bfec62bc9f20a16e1da78064722b938
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2026-01-09 01:11:00 +0300

    Time: 36 ms (90.26%) | Memory: 53.8 MB (58.17%) - LeetSync

---
+++ 2734-array-prototype-last/array-prototype-last.js
@@ -0,0 +1,23 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+
+ /*
+ [] - true
+ negative el - true
+
+ */
+Array.prototype.last = function() {
+    let n = this.length;
+
+    if (n === 0) {
+        return -1;
+    }else {
+        return this[n - 1];
+    }
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
