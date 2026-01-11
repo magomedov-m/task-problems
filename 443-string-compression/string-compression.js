3c07635bb4974aaf3640646772b0fe7a7155b7be
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2026-01-11 13:00:18 +0300

    Time: 3 ms (39.91%) | Memory: 57.9 MB (60.94%) - LeetSync

--- 443-string-compression/string-compression.js
+++ 443-string-compression/string-compression.js
@@ -2,60 +2,54 @@
  * @param {character[]} chars
  * @return {number}
  */
-/*
-Q:
- 1. sorted - false
- 2. only eng char - false
- 3. chars len = 0 - false
 
-A:
- two pointers
+ /*
+ en letters low/up case, num, symbol - true
+ [] - true
+ [a a a b b a a c a] - false
 
- a b b c
- ^
- w 
-    ^
-
- a a b b c c c
- ^
-     ^
-   w
- 
- a b 1 1 b b b b b b b b c
-                         ^
-                         ^
-         w
-*/
+ O(n)
+ O(n)
+ */
 var compress = function(chars) {
-    let l = 0;
-    let r = 0;
+    if (chars.length < 2) return 1;
 
-    while (r < chars.length) {
-        let current = chars[r];
-        let count = 0;
+    let s = '';
+    let count = 1;
 
-        while (r < chars.length && chars[r] === current) {
-            count++;
-            r++;
+    for (let i = 1; i < chars.length; i++) {
+        if (chars[i] !== chars[i - 1]) {
+            s += chars[i - 1];
+
+            if (count > 1) {
+                s += count;
+            }
+
+            count = 0;
         }
 
-        chars[l++] = current;
+        count++;
+    }
 
+    if (count > 0) {
+        s += chars[chars.length - 1];
+        
         if (count > 1) {
-            let countStr = count.toString();
-
-            for (let i = 0; i < countStr.length; i++) {
-                chars[l++] = countStr[i];
-            }
+            s += count;
         }
     }
 
-    return l;
-}
+    for (let j = 0; j < s.length; j++) {
+        chars[j] = s[j];
+    }
+
+    return s.length;
+};
+
 /*
-test:
- a 2 b 2 c c c
-         ^
-             ^
-           w
+s = ab2
+count = 2
+
+a b b
+    i
 */
