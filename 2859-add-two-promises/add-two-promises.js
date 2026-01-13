24044a509dd7371e4fde780aaf408c74a83d93b6
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2026-01-13 12:17:48 +0300

    Time: 51 ms (74.59%) | Memory: 54.4 MB (62.84%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -4,10 +4,10 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    const first = await promise1;
-    const second = await promise2;
+    let val1 = await promise1;
+    let val2 = await promise2;
 
-    return first + second;
+    return val1 + val2
 };
 
 /**
