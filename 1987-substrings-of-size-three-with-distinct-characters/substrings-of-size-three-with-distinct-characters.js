92bea7fac584e148ca835eb5cdc8c0928d835ec8
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2026-01-24 17:24:12 +0300

    Time: 1 ms (77.25%) | Memory: 54.5 MB (67.84%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.js
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.js
@@ -2,27 +2,38 @@
  * @param {string} s
  * @return {number}
  */
-var countGoodSubstrings = function(s) {
-    if (s.length < 3) return 0;
+ /*
+ s < 3 true
+ lowercase en letters
 
+ */
+var countGoodSubstrings = function(s) {
+    let mp = new Map();
+
+    let l = 0;
     let count = 0;
 
-    let left = 0;
-    let freq = {};
+    for (let r = 0; r < s.length; r++) {
+        mp.set(s[r], (mp.get(s[r]) || 0) + 1);
 
-    for (let right = 0; right < s.length; right++) {
-        freq[s[right]] = (freq[s[right]] || 0) + 1;
+        if (r - l + 1 > 3) {
+            mp.set(s[l], mp.get(s[l]) - 1);
+            if (mp.get(s[l]) === 0) mp.delete(s[l]);
+            l++;
+        }
 
-        if (right - left + 1 == 3) {
-            if (Object.keys(freq).length === 3) count++;
-
-            freq[s[left]]--;
-            if (freq[s[left]] === 0) {
-                delete freq[s[left]]
-            }
-            left++
+        if (mp.size === 3) {
+            count++;
         }
     }
 
     return count;
 };
+
+/*
+mp = [b:1, ]
+count = 0
+s = "aababcabc"
+      l
+        r
+*/
